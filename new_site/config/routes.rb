fbdca578999ccd0102cad652443018e36dc9cf40
jin ruan <jruan6377@bths.edu>
2014-08-17 15:17:55 -0400

    added stufff

--- new_site/config/routes.rb
+++ new_site/config/routes.rb
@@ -1,5 +1,7 @@
 NewSite::Application.routes.draw do
+  root :to=>'mentors#homepage'
   resources :mentors
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
