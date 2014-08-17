ffe7694a7917ce42c9c61f2c4d762327edafd0c5
jin ruan <jruan6377@bths.edu>
2014-08-17 13:10:59 -0400

    set up env

---
+++ mentor_site/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MentorSite::Application.config.secret_key_base = '581d1e2526e970bbc4589886f56ad0a95b1967db7b7fed95d1322b69ef5c3ab59381f7f9bacd070269bd34ad888f467a28ef996532c06471ca2168b37def8441'
